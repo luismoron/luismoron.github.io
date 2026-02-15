b0d28a487b275558fb65be1938d8648b01c06922
Luis Morón <luisleonelmoron@gmail.com>
2026-02-14 22:17:00 -0400

    feat: Inicializa la página de inicio del portafolio, actualiza el dominio a luismoron.dev y añade robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luismoron.github.io',
+  site: 'https://luismoron.dev',
   vite: {
     resolve: {
       alias: {
